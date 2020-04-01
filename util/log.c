c75aa716c6312014f2d3a09b4a00311705239470
lukasostendorf <lukas.ostendorf@gmail.com>
2020-04-01 13:52:34 +0200

    LOG: added makros to get simple performance measurements

--- util/log.c
+++ util/log.c
@@ -6,6 +6,7 @@
  */
 
 #include "log.h"
+#include <time.h>
 
 // Matlab log helper functions
 void log_matlab_fc(float complex* cpx, int num_samps, char* filename)
@@ -58,4 +59,25 @@ void log_bin(uint8_t* buf, uint buf_len, char* filename, char* mode)
 	}
 	fwrite(buf,buf_len,1, fd);
 	fclose(fd);
+}
+
+void timecheck_stop(struct timecheck_s* time)
+{
+    struct timespec end;
+    clock_gettime(CLOCK_MONOTONIC,&end);
+    float elapsed = (end.tv_sec-time->start.tv_sec)*1000000.0 +(end.tv_nsec-time->start.tv_nsec)/1000.0;
+    time->avg += elapsed;
+    time->count++;
+    if (elapsed>time->max)
+        time->max = elapsed;
+}
+
+void timecheck_info(struct timecheck_s* time)
+{
+    if (time->count%time->avg_len==0) {
+        LOG(WARN, "TimingMon '%20s:' avg:%4.3fus max:%4.3f after %6d iter\n",time->name,time->avg/time->avg_len,
+                time->max,time->count);
+        time->avg = 0;
+        time->max = 0;
+    }
 }
--- util/log.h
+++ util/log.h
@@ -18,7 +18,10 @@
 enum {TRACE, DEBUG,INFO,WARN,ERR,NONE};
 
 // Set the global log level
-#define LOG_LEVEL WARN
+#define LOG_LEVEL INFO
+
+// Enable/Disable timing performance evaluation
+#define TIMING_ENABLE
 
 // log macros with specified log level
 #define LOG(level,...) do { if (level>=LOG_LEVEL) \
@@ -37,10 +40,37 @@ enum {TRACE, DEBUG,INFO,WARN,ERR,NONE};
 #define LOG_BIN(level,x,y,z) do { if (level>=LOG_LEVEL) \
 										{ log_bin(x,y,z); } } while(0);
 
+struct timecheck_s {
+    char name[80];
+    float avg;
+    float max;
+    int count;
+    int avg_len;
+    struct timespec start;
+};
 
 void log_matlab_fc(float complex* cpx, int num_samps, char* filename);
 void log_matlab_f(float* floats, int num_samps, char* filename);
 void log_matlab_i(int* buf, int num_samps, char* filename);
 void log_bin(uint8_t* buf, uint buf_len, char* filename, char* mode);
+void timecheck_stop(struct timecheck_s* time);
+void timecheck_info(struct timecheck_s* time);
+
+#ifdef TIMING_ENABLE
+#define TIMECHECK_CREATE(obj); struct timecheck_s* obj;
+#define TIMECHECK_INIT(obj,objname,avglen) do { obj = calloc(sizeof(struct timecheck_s),1); \
+                                        memcpy(obj->name,objname,strlen(objname)); \
+                                        obj->avg_len=avglen; } while (0)
+#define TIMECHECK_START(obj) do { clock_gettime(CLOCK_MONOTONIC,&obj->start); \
+                                    } while(0)
+#define TIMECHECK_STOP(obj) timecheck_stop(obj)
+#define TIMECHECK_INFO(obj) timecheck_info(obj)
+#else
+#define TIMECHECK_CREATE(name)
+#define TIMECHECK_INIT(obj,name)
+#define TIMECHECK_START(name)
+#define TIMECHECK_STOP(name)
+#define TIMECHECK_INFO(name)
+#endif
 
 #endif /* UTIL_LOG_H_ */
