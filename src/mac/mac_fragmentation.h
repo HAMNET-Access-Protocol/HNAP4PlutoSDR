8672b6992012cfe836ea31460aa9f2c7a5ca9b83
lukasostendorf <lukas.ostendorf@gmail.com>
2020-06-17 09:52:29 +0200

    Include License Headers

--- src/mac/mac_fragmentation.h
+++ src/mac/mac_fragmentation.h
@@ -1,8 +1,19 @@
 /*
- * mac_fragmentation.h
+ * HNAP4PlutoSDR - HAMNET Access Protocol implementation for the Adalm Pluto SDR
  *
- *  Created on: Jan 13, 2020
- *      Author: lukas
+ * Copyright (C) 2020 Lukas Ostendorf <lukas.ostendorf@gmail.com>
+ *                    and the project contributors
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; version 3.0.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this library;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
  */
 
 #ifndef MAC_MAC_FRAGMENTATION_H_
--- src/mac/tap_dev.c
+++ src/mac/tap_dev.c
@@ -1,8 +1,19 @@
 /*
- * mac_forward_ether.c
+ * HNAP4PlutoSDR - HAMNET Access Protocol implementation for the Adalm Pluto SDR
  *
- *  Created on: Feb 10, 2020
- *      Author: lukas
+ * Copyright (C) 2020 Lukas Ostendorf <lukas.ostendorf@gmail.com>
+ *                    and the project contributors
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; version 3.0.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this library;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
  */
 
 #include "tap_dev.h"
