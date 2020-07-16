0cf92d448613b1dffa8107f8d48d83eb6b76174f
lukasostendorf <lukas.ostendorf@gmail.com>
2020-07-16 11:05:20 +0200

    LLDP: transmit firmware and HNAP version (#5)

--- startup_scripts/S90transceiver.sh
+++ startup_scripts/S90transceiver.sh
@@ -7,9 +7,13 @@ FILTER=AD9361_256kSPS.ftr
 
 # Config Link Layer Discovery Protocol to broadcast callsign
 # This has to be done after lldp is started (S60)
-CALLSIGN=`fw_printenv -n hnap_callsign`
+CALLSIGN=$(fw_printenv -n hnap_callsign)
+PLATFORM_STRING=$(grep plutosdr-fw /root/VERSION)
+DESC_STRING="$CALLSIGN $(grep hnap /root/VERSION | cut -c6-)"
+
 lldpcli configure lldp tx-interval 60
-lldpcli configure system description $CALLSIGN
+lldpcli configure system platform "$PLATFORM_STRING"
+lldpcli configure system description "$DESC_STRING"
 lldpcli resume
 
 # Install custom FIR filter
