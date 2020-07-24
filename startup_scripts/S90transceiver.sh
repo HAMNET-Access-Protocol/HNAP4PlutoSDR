c6140b64614e4ca2cb3b5005d174ddd51b920451
lukasostendorf <44869119+lukasostendorf@users.noreply.github.com>
2020-07-24 21:42:16 +0200

    LLDP: transmit firmware and HNAP version (#5) (#30)
    * LLDP: transmit firmware and HNAP version (#5)
    
    * LLDP: use hostname instead of platform for system description
    
    Platform parameter is only broadcasted in Cisco Discovery Protocol, not
    in LLDP
    
    * Callsign transmission: use CDP instead of LLDP

--- startup_scripts/S90transceiver.sh
+++ startup_scripts/S90transceiver.sh
@@ -7,9 +7,13 @@ FILTER=AD9361_256kSPS.ftr
 
 # Config Link Layer Discovery Protocol to broadcast callsign
 # This has to be done after lldp is started (S60)
-CALLSIGN=`fw_printenv -n hnap_callsign`
+CALLSIGN=$(fw_printenv -n hnap_callsign)
+PLATFORM_STRING=$(grep plutosdr-fw /root/VERSION)
+DESC_STRING="$(grep hnap /root/VERSION) $PLATFORM_STRING"
+
 lldpcli configure lldp tx-interval 60
-lldpcli configure system description $CALLSIGN
+lldpcli configure system hostname "$CALLSIGN"
+lldpcli configure system platform "$DESC_STRING"
 lldpcli resume
 
 # Install custom FIR filter
