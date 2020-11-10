73df24ac9c345d65d4effba2baadec0a65eea32d
lukasostendorf <lukas.ostendorf@gmail.com>
2020-11-10 17:28:52 +0100

    clean up code

--- src/runtime/client.c
+++ src/runtime/client.c
@@ -351,13 +351,14 @@ void phy_carrier_sync(PhyUE phy, platform hw) {
   pluto_set_txgain(hw, txgain);
 
   // tune to the correct frequency
-  pluto_set_tx_freq(hw,
-                    ul_lo + (long long)(cfo_hz * (float)ul_lo / (float)dl_lo));
-  pluto_set_rx_freq(hw, dl_lo + (long long)cfo_hz);
+  long long int ul_lo_fixed =
+      ul_lo + (long long)(cfo_hz * (float)ul_lo / (float)dl_lo);
+  long long int dl_lo_fixed = dl_lo + (long long)cfo_hz;
+  pluto_set_tx_freq(hw, ul_lo_fixed);
+  pluto_set_rx_freq(hw, dl_lo_fixed);
   LOG(INFO, "[CLIENT] retune transceiver with cfo %.3fHz:\n", cfo_hz);
-  LOG(INFO, "[CLIENT] TX LO freq: %lldHz\n",
-      ul_lo + (long long)(cfo_hz * (float)ul_lo / (float)dl_lo));
-  LOG(INFO, "[CLIENT] RX LO freq: %lldHz\n", dl_lo + (long long)cfo_hz);
+  LOG(INFO, "[CLIENT] TX LO freq: %lldHz\n", ul_lo_fixed);
+  LOG(INFO, "[CLIENT] RX LO freq: %lldHz\n", dl_lo_fixed);
   LOG(INFO, "[CLIENT] rxgain adjusted: %d\n", rxgain);
   LOG(INFO, "[CLIENT] txgain adjusted: %d\n", txgain);
   free(rxbuf_time);
